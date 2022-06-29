bb871d0d111ce15024f4cbe2e4ce5d813a6f9f87
muhammadriaz0505 <muhammadriaz11446@gmail.com>
2022-06-29 18:09:20 -0400

    Java Class 24

---
+++ src/com/syntax/class24/StudentTester.java
@@ -0,0 +1,17 @@
+package com.syntax.class24;
+
+public class StudentTester {
+    public static void main(String[] args) {
+        Student [] students={new SyntaxStudent(),new SchoolStudent(),new CollegeStudents()};
+        for(Student st:students){
+            st.study();
+            st.practice();
+            st.doHomeWork();
+        }
+        for (int i = 0; i <students.length ; i++) {
+            students[i].study();
+            students[i].practice();
+            students[i].doHomeWork();
+        }
+    }
+}
